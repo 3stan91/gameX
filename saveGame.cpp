98c168caae7a6f67bd06f2d057c6188ec4a79662
Alexandr Tristinskiy <3stan91@gmail.com>
2023-05-09 19:09:20 +0600

    change saveGame

--- saveGame.cpp
+++ saveGame.cpp
@@ -4,17 +4,16 @@
 #include <string>
 
 void saveGame(Creature creature, std::string path) {
-/*
+
     if (creature.getName().find("Enemy") != std::string::npos) {
         std::ofstream fout(path, std::ios::binary | std::ios::app);
         if (fout.good()) {
             fout.write(creature.getName().c_str(), sizeof(creature.getName()))<<std::endl;                             //name
-            fout.write(reinterpret_cast<const char *>(creature.getHealth()), sizeof(creature.getHealth()))<<std::endl;  //health
-            fout.write(reinterpret_cast<const char *>(creature.getDamage()),
-                       sizeof(creature.getDamage()))<<std::endl;   //damage
-            fout.write(reinterpret_cast<const char *>(creature.getArmor()), sizeof(creature.getArmor()))<<std::endl;
-            fout.write(reinterpret_cast<const char *>(creature.coordinate.x), sizeof(creature.coordinate.x))<<std::endl;
-            fout.write(reinterpret_cast<const char *>(creature.coordinate.y), sizeof(creature.coordinate.y))<<std::endl;
+            fout.write((char *) creature.getHealth(), sizeof(creature.getHealth()))<<std::endl;  //health
+            fout.write((char *) creature.getDamage(), sizeof(creature.getDamage()))<<std::endl;   //damage
+            fout.write((char *) creature.getArmor(), sizeof(creature.getArmor()))<<std::endl;
+            fout.write((char *) creature.coordinate.x, sizeof(creature.coordinate.x))<<std::endl;
+            fout.write((char *) creature.coordinate.y, sizeof(creature.coordinate.y))<<std::endl;
         } else
             throw "File \"Enemies.bin\" is not access\n";
     } else {
@@ -31,5 +30,4 @@ void saveGame(Creature creature, std::string path) {
             throw "File \"Personage.bin\" is not access\n";
         }
     }
-*/
  }
